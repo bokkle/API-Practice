0bbb442e25ab9849574b3dfe3c4cf16652457ccd
bokkle <mitch.r_@hotmail.com>
2023-04-26 21:31:22 -0400

    API prac

---
+++ user.js
@@ -0,0 +1,25 @@
+const postListEl = document.querySelector('.post-list')
+const id = localStorage.getItem("id")
+
+const onSearchChange = async (event) => {
+    const id = event.target.value
+    renderPosts(id)
+}
+
+const renderPosts = async (id) => {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json()
+    postListEl.innerHTML =  postsData.map(post => postHTML(post)).join('')
+}
+
+const postHTML = (post) => {
+    return `<div class="post">
+    <div class="post__title">
+      ${post.title}
+    </div>
+    <p class="post__body">
+      ${post.body}
+    </p>
+  </div>`
+}
+renderPosts(id)
